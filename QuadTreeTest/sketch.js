9c5bfc4fdac21abb5362aac9564f771084e8d32a
Nicolas <nicdelgado03@Gmail.com>
2025-10-29 22:29:44 -0500

    Upload Test Quad Tree

---
+++ QuadTreeTest/sketch.js
@@ -0,0 +1,21 @@
+let qtree;
+
+function setup() {
+    createCanvas(400,400);
+
+    let boundary = new Rectangle(200, 200, 200, 200);
+    qtree = new QuadTree(boundary, 4);
+    console.log(qtree);
+}
+
+
+function draw() {
+    if (mouseIsPressed) {
+        for (let i = 0; i < 5; i++) {
+            let m = new Point(mouseX + random(-5, 5), mouseY + random(-5, 5));
+            qtree.insert(m);
+        }
+    background(0);
+    qtree.show();
+}
+}
